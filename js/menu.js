17c1d1f5f97945128e9dd1924c7c6d6ec13de88e
Daniel Suchan <mr.sucik@gmail.com>
2022-05-08 15:07:32 +0800

    Initial files

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+﻿$(document).scroll(function () {
+    var nav = document.querySelector("nav");
+    //var ser = document.getElementsByTagName("a")[4];
+    //var pro = document.getElementsByTagName("a")[3];
+    //var pri = document.getElementsByTagName("a")[2];
+    //var con = document.getElementsByTagName("a")[1];
+    //var bottom = $(window).height() - ser.height();
+    
+    if ($(document).scrollTop() > 5)
+        nav.style.background = "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.3)";
+    else
+        nav.style.background = "rgba(0, 0, 0, 0)";
+});
+var isVis = function (elem) {
+
+}
---
+++ js/scrolling.js
@@ -0,0 +1,27 @@
+﻿$("body").on("scroll", function () {
+    console.log("haha");
+});
+function scrolla(a) {
+    if (navigator.userAgent.search("Firefox") >= 0) {
+        ScrollToResolver(document.getElementById(a));
+    }
+    else {
+        $("html, body").animate({
+            scrollTop: $("#" + a).offset().top - 70
+        }, 1400)
+    }
+}
+function ScrollToResolver(elem) {
+    var jump = parseInt(elem.getBoundingClientRect().top * .2);
+    document.body.scrollTop += jump;
+    document.documentElement.scrollTop += jump;
+    if (!elem.lastjump || elem.lastjump > Math.abs(jump)) {
+        elem.lastjump = Math.abs(jump);
+        setTimeout(function () {
+            ScrollToResolver(elem);
+        }, "30");
+    }
+    else {
+        elem.lastjump = null;
+    }
+}
