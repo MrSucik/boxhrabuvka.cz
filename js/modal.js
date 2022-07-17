5cd4b9e563aaf81fa696d8bd74dc3664910bada4
Daniel Suchan <mr.sucik@gmail.com>
2022-07-17 12:18:18 +0200

    Cleanup

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,16 @@
 ﻿// Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("myModal");
 
 // When the user clicks on <span> (x), close the modal
-document.getElementsByClassName("close")[0].onclick = function() {
-    modal.style.display = "none";
-}
+document.getElementsByClassName("close")[0].onclick = function () {
+  modal.style.display = "none";
+};
 function openModal() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
